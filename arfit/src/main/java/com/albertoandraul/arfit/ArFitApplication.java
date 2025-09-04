a95619a8d19d4cb6828f5e9f22f28035fe83ef35
Raul <raulgranadostbgmail.com>
2025-09-04 19:08:20 +0200

    Primer commit: proyecto inicial

---
+++ arfit/src/main/java/com/albertoandraul/arfit/ArFitApplication.java
@@ -0,0 +1,13 @@
+package com.albertoandraul.arfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArFitApplication.class, args);
+	}
+
+}
---
+++ arfit/src/test/java/com/albertoandraul/arfit/ArFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoandraul.arfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
